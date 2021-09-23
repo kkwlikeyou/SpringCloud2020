a1c48fc1af654a175402dc02bfc90b2d0c47cc44
kkwli <kkwlikeyou@gmail.com>
2021-09-23 21:20:05 +0800

    使用 MySelfRule.java ,注入IRule的RandomRule实现类 来实现随机负载均衡

--- cloud-consumer-order80/src/main/java/com/kkwli/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/kkwli/springcloud/OrderMain80.java
@@ -1,11 +1,14 @@
 package com.kkwli.springcloud;
 
+import com.kkwli.myrule.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableDiscoveryClient /*开启加入注册中心*/
+//@RibbonClient(name = "cloud-payment-service",configuration = MySelfRule.class) /*Ribbon负载规则替换*/
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class, args);
--- cloud-consumer-order80/src/main/java/com/kkwli/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/kkwli/springcloud/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.kkwli.springcloud.controller;
 import com.kkwli.springcloud.entities.CommonResult;
 import com.kkwli.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,6 +27,25 @@ public class OrderController {
 
     @GetMapping("consumer/payment/get/{id}")
     public CommonResult<Payment> getPayment(@PathVariable("id") Long id) {
-        return restTemplate.getForObject(PAYMENT_URL+"/payment/get/"+id,CommonResult.class);
+        return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
     }
+
+    @GetMapping("/consumer/payment/getForEntity/{id}")
+    public CommonResult getForEntity(@PathVariable("id") Long id) {
+        ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+        if (entity.getStatusCode().is2xxSuccessful()) {
+            return entity.getBody();
+        }
+        return new CommonResult<>(444,"操作失败");
+    }
+
+    @GetMapping("/consumer/payment/createForEntity")
+    public CommonResult createForEntity(Payment payment) {
+        ResponseEntity<CommonResult> entity = restTemplate.postForEntity(PAYMENT_URL + "/payment/create", payment, CommonResult.class);
+        if (entity.getStatusCode().is2xxSuccessful()) {
+            return entity.getBody();
+        }
+        return new CommonResult<>(444, "操作失败");
+    }
+
 }
