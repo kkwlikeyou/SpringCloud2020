74121c4ff8226420bcb5b2bda76658acf9b321b3
kkwli <kkwlikeyou@gmail.com>
2021-09-23 16:08:17 +0800

    完成OrderMain80 和 PaymentMain8004 使用WLS中的zookeeper

---
+++ cloud-provider-payment8004/src/main/java/com/kkwli/springcloud/controller/PaymentController.java
@@ -0,0 +1,22 @@
+package com.kkwli.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@Slf4j
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @RequestMapping("/payment/zk")
+    public String paymentZk() {
+        return "springCloud with zookeeper:" + serverPort + "\t" + UUID.randomUUID();
+    }
+
+}
