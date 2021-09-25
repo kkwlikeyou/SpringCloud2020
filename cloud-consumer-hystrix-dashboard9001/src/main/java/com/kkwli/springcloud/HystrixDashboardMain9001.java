cb5cd32b33e2d2f51124460ef833acde3562f401
kkwli <kkwlikeyou@gmail.com>
2021-09-25 15:58:28 +0800

    63_Hystrix图形化Dashboard搭建

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/kkwli/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,17 @@
+package com.kkwli.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @Author kkwli
+ * @Since 2021/9/25 15:54
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class,args);
+    }
+}
