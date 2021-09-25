1aea34dcd46356c310c92f0adc13c4c6bcd69274
kkwli <kkwlikeyou@gmail.com>
2021-09-25 16:14:14 +0800

    修改Bean

--- cloud-provider-hystrix-payment8001/src/main/java/com/kkwli/springcloud/PaymentHystrixMain8001.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/kkwli/springcloud/PaymentHystrixMain8001.java
@@ -1,9 +1,12 @@
 package com.kkwli.springcloud;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @version 1.0
@@ -17,5 +20,15 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class PaymentHystrixMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentHystrixMain8001.class,args);
+    }
+
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean<HystrixMetricsStreamServlet> registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
     }
 }
