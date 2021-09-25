85517c6ecf821ae2830441afe0d599dfe353c17a
kkwli <kkwlikeyou@gmail.com>
2021-09-25 13:10:56 +0800

    54_Hystrix值服务降级支付侧fallback

--- cloud-provider-hystrix-payment8001/src/main/java/com/kkwli/springcloud/PaymentHystrixMain8001.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/kkwli/springcloud/PaymentHystrixMain8001.java
@@ -2,6 +2,7 @@ package com.kkwli.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
@@ -12,6 +13,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableCircuitBreaker
 public class PaymentHystrixMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentHystrixMain8001.class,args);
--- cloud-provider-hystrix-payment8001/src/main/java/com/kkwli/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/kkwli/springcloud/service/PaymentService.java
@@ -1,5 +1,7 @@
 package com.kkwli.springcloud.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -17,13 +19,23 @@ public class PaymentService {
         return "线程池:" + Thread.currentThread().getName() + "\tpaymentInfo_OK,id:" + id + "\t哈哈";
     }
 
+    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler",commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds" , value = "1000")
+    })
     public String paymentInfo_TimeOut(Integer id) {
-        try {
-            TimeUnit.SECONDS.sleep(3);
+        int timeNumber = 5;
+        int age = 10/0;
+        /*try {
+
+            TimeUnit.SECONDS.sleep(timeNumber);
         } catch (InterruptedException e) {
             e.printStackTrace();
-        }
-        return "线程池:" + Thread.currentThread().getName() + "\tpaymentInfo_TimeOut,id:" + id + "\t哈哈,耗时3秒";
+        }*/
+        return "线程池:" + Thread.currentThread().getName() + "\tpaymentInfo_TimeOut,id:" + id + "\t哈哈,耗时" + timeNumber + "秒";
+    }
+
+    public String paymentInfo_TimeOutHandler(Integer id) {
+        return "线程池:" + Thread.currentThread().getName() + "\tpaymentInfo_TimeOutHandler,id:" + id + "\t(っ °Д °;)っ";
     }
 
 }
