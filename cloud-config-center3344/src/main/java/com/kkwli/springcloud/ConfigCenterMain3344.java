d1d6bf8fd9dd10f1363dada275fe539d9854dd2f
kkwli <kkwlikeyou@gmail.com>
2021-09-26 15:09:05 +0800

    feat:

---
+++ cloud-config-center3344/src/main/java/com/kkwli/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,17 @@
+package com.kkwli.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @Author kkwli
+ * @Since 2021/9/26 14:22
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+}
